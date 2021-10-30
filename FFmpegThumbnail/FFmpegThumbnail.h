bf96f1c096fc6412b10b864cb4db42fc0f7d3b8f
megakraken <15066925+megakraken@users.noreply.github.com>
2021-10-30 19:29:17 +0200

    documentation

--- FFmpegThumbnail/FFmpegThumbnail.h
+++ FFmpegThumbnail/FFmpegThumbnail.h
@@ -21,13 +21,42 @@
 
 #include <Windows.h>
 
+/**
+ * Generates thumbnail from first frame of the video.
+ */
 #define TS_FIRSTFRAME   0x80000001
+/**
+ * Generates thumbnail after first few seconds of video.
+ */
 #define TS_BEGINNING    0x80000002
+/**
+ * Generates thumbnail from the middle of the video.
+ */
 #define TS_MIDDLE       0x80000003
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+    /**
+     * Gets a thumbnail for the video contained in the given stream.
+     *
+     * @param stream  A pointer to an IStream interface that represents the stream
+     *                source that contains the video.
+     * @param cx      The maximum thumbnail size, in pixels. The returned bitmap should
+     *                fit into a square of width and height cx, though it does not need
+     *                to be a square image.
+     * @param ts      The timestamp at which to generate the thumbnail within the video
+     *                file. Can be either a positive value to denote an offset in seconds,
+     *                or one of the following values:
+     *                TS_FIRSTFRAME - Generate thumbnail from the very first video frame.
+     *                TS_BEGINNING  - Generate thumbnail from the beginning of the video.
+     *                TS_MIDDLE     - Generate thumbnail from the middle of the video.
+     * @param hbmp    When this function returns, contains a pointer to the thumbnail
+     *                image handle. The image is a DIB section and 32 bits per pixel.
+     *
+     * @return        If this function succeeds, it returns S_OK. Otherwise, it returns
+     *                an HRESULT (or FFMPEG) error code.
+     */
     HRESULT GetThumbnail(IN IStream *stream, int cx, int ts, OUT HBITMAP *thumbnail);
 #ifdef __cplusplus
 }
