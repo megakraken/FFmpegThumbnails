86fa4cd85510f594b04218f8c5348a12f485a2ff
megakraken <15066925+megakraken@users.noreply.github.com>
2024-01-10 16:30:13 +0100

    Statically link CRT to remove dependency on runtime DLLs

--- Configure/Properties/AssemblyInfo.cs
+++ Configure/Properties/AssemblyInfo.cs
@@ -9,8 +9,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Configuration Utility for FFmpegThumbnailProvider")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("megakraken")]
-[assembly: AssemblyProduct("FFmpegThumbnailProvider 1.0 (Based on FFmpeg n4.4.1)")]
-[assembly: AssemblyCopyright("Copyright © megakraken 2021")]
+[assembly: AssemblyProduct("FFmpegThumbnailProvider 1.1 (Based on FFmpeg n4.4.1)")]
+[assembly: AssemblyCopyright("Copyright © megakraken 2021 - 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
